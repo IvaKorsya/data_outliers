886f8e5394d90a61084f97334387730c7c86c627
BelowD <belov.dv@edu.spbstu.ru>
2025-04-15 23:56:31 +0300

    Update __init__.py

--- core/detectors/__init__.py
+++ core/detectors/__init__.py
@@ -1,18 +1,3 @@
-# detectors/__init__.py
-from .activity_spikes import ActivitySpikesDetector
-from .isolation_forest import IsolationForestDetector
-from .night_activity import NightActivityDetector
-from .node_id_check import NodeIdChecker
-from .page_view import PageViewAnomalyDetector
-from .untagged_bots import UntaggedBotsDetector
 from .test_detector import TestDetector
 
-__all__ = [
-    'ActivitySpikesDetector',
-    'IsolationForestDetector',
-    'NightActivityDetector',
-    'NodeIdChecker',
-    'PageViewAnomalyDetector',
-    'UntaggedBotsDetector',
-    'TestDetector'
-]
+__all__ = ['TestDetector']
