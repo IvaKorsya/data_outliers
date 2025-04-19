1226ef95fe0b3b1c15ba954ad5d6c66da4818cb4
BelowD <belov.dv@edu.spbstu.ru>
2025-04-19 21:09:55 +0000

    Pending changes exported from your codespace

--- core/detectors/__init__.py
+++ core/detectors/__init__.py
@@ -6,9 +6,11 @@ __all__ = ['DataLoader', 'ReportGenerator', 'AnalysisRunner']
 # core/detectors/__init__.py
 from .activity_spikes import ActivitySpikesDetector
 from .node_id_check import NodeIdCheckDetector
+from .page_view import PageViewOrderDetector
 
 __all__ = [
     'ActivitySpikesDetector',
-    'NodeIdCheckDetector'
+    'NodeIdCheckDetector',
+    'PageViewOrderDetector'
 ]
 
