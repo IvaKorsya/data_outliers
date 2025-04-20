c0dd2964624bfab148a5961196b6c5b9415e2cfe
BelowD <belov.dv@edu.spbstu.ru>
2025-04-20 21:07:42 +0000

    Pending changes exported from your codespace

--- core/detectors/__init__.py
+++ core/detectors/__init__.py
@@ -7,12 +7,14 @@ from .activity_spikes import ActivitySpikesDetector
 from .node_id_check import NodeIdCheckDetector
 from .page_view import PageViewOrderDetector
 from .untagged_bots import UntaggedBotsDetector
+from .isolation_forest import IsolationForestDetector
 
 
 __all__ = [
     'ActivitySpikesDetector',
     'NodeIdCheckDetector',
     'PageViewOrderDetector',
-    'UntaggedBotsDetector'
+    'UntaggedBotsDetector',
+    'IsolationForestDetector'
 ]
 
