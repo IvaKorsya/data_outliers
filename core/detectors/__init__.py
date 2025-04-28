517c5330b5074ec8f27095bae01c23bb112927bd
BelowD <belov.dv@edu.spbstu.ru>
2025-04-29 01:02:14 +0300

    Update __init__.py

--- core/detectors/__init__.py
+++ core/detectors/__init__.py
@@ -1,8 +1,9 @@
+# core/detectors/__init__.py
 from core.data_loader import DataLoader
 from core.report_generator import ReportGenerator
 from core.runner import AnalysisRunner
 __all__ = ['DataLoader', 'ReportGenerator', 'AnalysisRunner']
-# core/detectors/__init__.py
+
 from .activity_spikes import ActivitySpikesDetector
 from .node_id_check import NodeIdCheckDetector
 from .page_view import PageViewOrderDetector
